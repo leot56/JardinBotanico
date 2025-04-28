94c31d29ee2fc8354e28e47cb1cdb07a7396d76d
GabrielaG7 <gabrielagomezsc@gmail.com>
2025-04-28 20:21:39 -0300

    Update base.pl

--- base.pl
+++ base.pl
@@ -52,10 +52,10 @@ florece_primavera(Planta):- planta(Planta, epoca(floracion, primavera)).
 tiene_color(Planta, Color) :- planta(Planta, color(Color)).
 
 %2
-requiere_riego_especial(Planta):- tipo_arbusto(Planta), planta(Planta, epoca(floracion, verano).
+requiere_riego_especial(Planta):- tipo_arbusto(Planta), planta(Planta, epoca(floracion, verano)).
 atrae_insectos_beneficos(Planta):- tiene_color(Planta, amarillo).
 atrae_insectos_beneficos(Planta):- tiene_color(Planta, rojo).
-considerada_alta(Planta):- not(planta(Planta, tipo(flor)).
+considerada_alta(Planta):- not(planta(Planta, tipo(flor))).
 
 
 % 3. Plantas Cortas y Flores:
