fefdbc865eb84579885344271f812138a50f6eec
GabrielaG7 <gabrielagomezsc@gmail.com>
2025-05-22 23:00:42 -0300

    Update test.pl
    Corrección de los tests de los puntos 2 y 3

--- test.pl
+++ test.pl
@@ -3,13 +3,13 @@
 :- begin_tests(jardin_botanico).
 
 % Tests para el punto 1
-
+%a
 test(rose_es_arbusto) :-
     tipo_arbusto(rose).
 
 test(tulip_no_es_arbusto, fail) :-
     tipo_arbusto(tulip).
-
+%b
 test(rose_florece_en_primavera) :-
     florece_primavera(rose).
 
@@ -18,7 +18,7 @@ test(lily_no_florece_en_primavera, fail) :-
 
 test(plantas_que_florecen_en_primavera, set(Planta == [orchid, rose, tulip])) :-
     florece_primavera(Planta).
-
+%c
 test(rose_tiene_color_rojo) :-
     tiene_color(rose, rojo).
 
@@ -27,28 +27,47 @@ test(el_color_de_rose_no_es_amarillo, fail) :-
 
 
 % Tests para el punto 2
-
+%a
 test(ninguna_planta_necesita_riego_especial, fail) :-
-    requiere_riego_especial(_).
+    planta(_, requiere(riego_especial)).
 
+%b
 test(plantas_que_atraen_insectos_beneficos, set(Planta == [rose, sunflower, tulip])) :-
-    atrae_insectos_beneficos(Planta).
+    planta(Planta, atrae(insectos_beneficos)).
 
-test(lily_no_atraen_insectos_beneficos, fail):-
-    atrae_insectos_beneficos(lily).
+test(lily_no_atrae_insectos_beneficos, fail):-
+    planta(lily, atrae(insectos_beneficos)).
 
-test(rose_planta_alta) :-
-    considerada_alta(rose).
+test(atrae_insectos_color_amarillo) :-
+    planta(sunflower, atrae(insectos_beneficos)).
+
+test(atrae_insectos_rojo) :-
+    planta(rose, atrae(insectos_beneficos)).
+%c
+test(bamboo_planta_alta) :-
+    planta(bamboo, considerada(alta)).
 
 test(lily_no_es_planta_alta, fail) :-
-    considerada_alta(lily).
+    planta(lily, considerada(alta)).
 
 %Tests Para el punto 3
-test(plantas_cortas_de_tipo_flor, set(Planta == [lily, orchid, tulip])) :-
-    plantas_cortas_flores(Planta).
     
-test(sunflower_no_corta_solo_flor, fail):-
-    plantas_cortas_flores(sunflower).
+test(rose_no_es_corta_y_ni_flor, fail) :-
+    corta_y_flor(rose).
+
+test(sunflower_no_es_corta_y_flor, fail) :-
+    corta_y_flor(sunflower). 
+
+test(plantas_que_son_cortas_y_flores, set(Planta == [lily, tulip, orchid])) :-
+    corta_y_flor(Planta).
+
+test(plantas_cortas_y_flor_no_es_flor, fail) :-
+    plantas_cortas_flores(Plantas),
+    member(cactus, Plantas).
+
+test(plantas_cortas_y_flor_tiene_tres_elementos) :-
+    plantas_cortas_flores(Plantas),
+    length(Plantas, 3).
 
 % Test para el punto 4
 test(hay_relacion_planta_observador) :-
